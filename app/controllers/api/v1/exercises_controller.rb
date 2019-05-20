df5eed2c5b5f84c2836c4673664ef283d1528edf
mnkillebr <killebrew.biz@gmail.com>
2019-05-20 13:14:32 -0400

    exercise seed

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ExercisesController < ApplicationController
+skip_before_action :authorized, only: [:index]
+
+  def index
+    @exercises = Exercise.all
+    render json: @exercises
+  end
+
 end
