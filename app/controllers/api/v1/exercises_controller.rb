7c8dd2993b4acb38949d7742a44e7fc17f5eba3d
mnkillebr <killebrew.biz@gmail.com>
2019-05-22 09:12:47 -0400

    ex controller done

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,9 +1,25 @@
 class Api::V1::ExercisesController < ApplicationController
-skip_before_action :authorized, only: [:index]
+skip_before_action :authorized#, only: [:index]
 
   def index
     @exercises = Exercise.all
     render json: @exercises
+  end
+
+  def create
+    @exercise = Exercise.create(exercise_params)
+    render json: @exercise
+  end
+
+  def destroy
+    @exercise = Exercise.find(params[:id])
+    @exercise.destroy
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :difficulty, :media, :description, :muscles)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :show, :update, :destroy]
       resources :user_workout_cards, only: [:create, :destroy]
-      resources :exercises, only: [:index, :show]
+      resources :exercises, only: [:index, :show, :create, :destroy]
       resources :workout_cards, only: [:index, :create, :show, :update, :destroy]
       resources :workouts, only: [:create, :destroy]
       post '/login', to: 'auth#create'
