69c891f511e219293a35a8a996497d2006b4c483
mnkillebr <killebrew.biz@gmail.com>
2019-05-22 10:35:22 -0400

    workout card controllers

--- app/controllers/api/v1/workout_cards_controller.rb
+++ app/controllers/api/v1/workout_cards_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::WorkoutCardsController < ApplicationController
+skip_before_action :authorized
+
+  def index
+    @workout_cards = WorkoutCard.all
+    render json: @workout_cards
+  end
+
 end
